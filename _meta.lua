bf1085853b97191c4e326d07a526346d56df1007
ömer faruk inel <omer.inel@ciellos.com>
2026-02-15 15:04:32 +0300

    v1.1.0

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "phrasedeck",
     fullname = _("PhraseDeck"),
     description = _([[Collect phrases from books and study them with spaced repetition flashcards.]]),
-    version = "1.0.0",
+    version = "1.1.0",
 }
